cf8ef87841dfbfa55c57e6c83be65b0d0139c20c
Angus20050510 <145432971+Angus20050510@users.noreply.github.com>
2023-11-07 15:20:44 +0800

    Update hw3-4.c

--- hw3-4.c
+++ hw3-4.c
@@ -3,27 +3,17 @@
 
 int main() {
     char answer[5];
-    char guess[5]; 
-    int A = 0, B = 0;
+    char guess[5];
+    int output[2];
     int ext;
     scanf("%s", answer);
     scanf("%s", guess);
     scanf("%d", &ext);
     if(ext==0){
-        for(int i = 0; i < 4; i++) {
-            if(guess[i] == answer[i]) {
-                A++;
-            } 
-            else {
-                for(int j = 0; j < 4; j++) {
-                    if(guess[i] == answer[j]) {
-                        B++;
-                    }
-                }
-            }
-        }
-    printf("%dA%dB\n", A, B);
+        game_guessnum(answer,guess,output);
+        printf("%dA%dB\n", output[0], output[1]);
     }
 
     return 0;
 }
+
